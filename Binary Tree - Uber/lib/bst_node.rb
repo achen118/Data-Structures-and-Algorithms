a20476dcca81bf0e2ac97a83d4f5bdc40ff7a186
Syrie Bianco <syriebianco@gmail.com>
2017-09-08 15:18:34 -0700

    Re-complete binary search tree

---
+++ Binary Tree - Uber/lib/bst_node.rb
@@ -0,0 +1,43 @@
+class BSTNode
+  attr_reader :value
+  attr_accessor :left, :right, :parent
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @left = nil
+    @right = nil
+  end
+
+  def less_than?(other_node)
+    @value <= other_node.value
+  end
+
+  def append_to(parent)
+    @parent = parent
+
+    less_than?(parent) ? parent.left = self : parent.right = self
+
+    value
+  end
+
+  def children
+    children = []
+
+    children << @left if @left
+    children << @right if @right
+
+    children
+  end
+
+  def sibling?
+    @parent.children.length == 2
+  end
+
+  def replace(deleted)
+    @parent = deleted.parent
+    @left, @right = deleted.left, deleted.right
+
+    @parent.left = self
+  end
+
+end
