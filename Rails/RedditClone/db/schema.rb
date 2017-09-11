c8127b7eaa505830badb3832de00a5ef9a3cc234
Syrie Bianco <syriebianco@gmail.com>
2017-09-11 11:20:08 -0700

    Migrate users, posts, and tags

--- Rails/RedditClone/db/schema.rb
+++ Rails/RedditClone/db/schema.rb
@@ -11,6 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170911181602) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "post_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["post_id"], name: "index_tags_on_post_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.string   "session_token",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
