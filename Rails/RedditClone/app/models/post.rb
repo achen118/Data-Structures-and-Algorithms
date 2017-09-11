5c444e5cf7181807344377035794d53ee3dfe0d4
Syrie Bianco <syriebianco@gmail.com>
2017-09-11 11:29:07 -0700

    Complete User, Tag, and Post models

--- Rails/RedditClone/app/models/post.rb
+++ Rails/RedditClone/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :user, :title, :body, presence: true
+
+  belongs_to :user
+  has_many :tags
 end
--- Rails/RedditClone/app/models/tag.rb
+++ Rails/RedditClone/app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ActiveRecord::Base
+  validates :post, :name, presence: true
+
+  belongs_to :post
 end
