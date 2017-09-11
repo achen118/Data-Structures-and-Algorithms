5ea150ea664f584c7c63d4c3af3d6699d951d911
Syrie Bianco <syriebianco@gmail.com>
2017-09-11 10:49:51 -0700

    Initialize Rails MusicApp

--- Binary Tree - Uber/lib/interview_questions.rb
+++ Binary Tree - Uber/lib/interview_questions.rb
@@ -1,17 +1,50 @@
+require_relative 'binary_search_tree'
+
+
+
+class BinarySearchTree
+
+  def lca(val1, val2)
+    ancestors
+
+  end
+end
+
+
 # Post Order Traversal:
 
 
 # Pre Order Traversal:
 
 
-# LCA: 
-# In a binary search tree, an *ancestor* of a `example_node` is a node 
-# that is on a higher level than `example_node`, and can be traced directly 
-# back to `example_node` without going up any levels. (I.e., this is 
-# intuitively what you think an ancestor should be.) Every node in a binary 
-# tree shares at least one ancestor -- the root. In this exercise, write a 
-# function that takes in a BST and two nodes, and returns the node that is the 
+# LCA:
+# In a binary search tree, an *ancestor* of a `example_node` is a node
+# that is on a higher level than `example_node`, and can be traced directly
+# back to `example_node` without going up any levels. (I.e., this is
+# intuitively what you think an ancestor should be.) Every node in a binary
+# tree shares at least one ancestor -- the root. In this exercise, write a
+# function that takes in a BST and two nodes, and returns the node that is the
 # lowest common ancestor of the given nodes. Assume no duplicate values.
 
 
 
+
+# test cases
+
+bst = BinarySearchTree.new
+[5, 3, 7, 1, 4, 9, 0, 2, 1.5, 10].each do |el|
+  bst.insert(el)
+end
+
+#############################
+# prefilled_bst looks like: #
+#             (5)           #
+#            /   \          #
+#          (3)   (7)        #
+#         /  \      \       #
+#      (1)   (4)    (9)     #
+#     /   \            \    #
+#   (0)   (2)          (10) #
+#        /                  #
+#      (1.5)                #
+#############################
