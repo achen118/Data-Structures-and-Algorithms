2684cca329026cecf686eec0bb262171dca64b21
Syrie Bianco <syriebianco@gmail.com>
2017-09-13 22:16:44 -0700

    Remove excess files

--- Currently In Progress/Rails/a04_skeleton/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-  validates :title, :body, :user, presence: true
-
-  has_many :tags
-  belongs_to :user
-end
--- Currently In Progress/Rails/a04_skeleton/config/routes.rb
+++
@@ -1,6 +0,0 @@
-PostsApp::Application.routes.draw do
-  resources :users, only: [:new, :create]
-  resource :sessions, only: [:new, :create, :destroy]
-  resources :links
-  resources :comments, only: [:create, :destroy]
-end
